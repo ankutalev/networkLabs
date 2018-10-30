65ec7fc149568bbc2a97c247ce6dfe01b9f5ec4a
akutalev <a.kutalev@g.nsu.ru>
2018-10-30 20:17:28 +0700

    another cleanup

--- Network1/main.cpp
+++ Network1/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include "MySocket.h"
 #include "ReplicateCounter.h"
+
 int main(int argc, char* argv[]) {
 
     std::string mltcstAddr = argc < 2 ? "224.0.1.15" : argv[1];
