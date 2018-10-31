3b0f61a9d284ad0e1eb0f0ced17f10115d4fcc0e
ankutalev <a.kutalev@g.nsu.ru>
2018-10-31 13:39:08 +0700

    Client code refactoring

--- Network2/Client.cpp
+++ Network2/Client.cpp
@@ -11,53 +11,65 @@
 int main(int argc , char *argv[]) {
     int sock;
     struct sockaddr_in server;
-    char message[4096] = "hehaheh.txt " , server_reply[2000];
+
+    if (argc < 2) {
+        std::cerr << "No file given!" << std::endl;
+        return -1;
+    }
+
+    static const int MAX_FILE_PATH_IN_UTF_8 = 4 * 4096;
+    static const int SERVER_REPLY_BUFFER_SIZE = 2000;
+
+    char filePath[MAX_FILE_PATH_IN_UTF_8];
+    char serverReply[SERVER_REPLY_BUFFER_SIZE];
+
+    std::fill(filePath, filePath + MAX_FILE_PATH_IN_UTF_8, 0);
+    std::fill(serverReply, serverReply + SERVER_REPLY_BUFFER_SIZE, 0);
+
+
     sock = socket(AF_INET , SOCK_STREAM, 0);
     if (sock == -1) {
-        printf("Could not create socket");
+        std::cerr << "Could not create socket" << std::endl;
+        return -1;
     }
-    puts("Socket created");
-
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(528491);
+    std::cout << "Trying to connect!" << std::endl;
 
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
+    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        std::cerr << "Connect failed" << std::endl;
         return 1;
     }
+    std::cout << "Connected" << std::endl;
 
-    puts("Connected\n");
 
-         struct stat st;
-        stat("heh.txt",&st);
-        std::string y (message);
-        auto x = std::to_string(st.st_size);
-        x+=" ";
-        x+="LOLKEK ";
-        y+=x;
+    struct stat st;
+    stat(argv[1], &st);
+    std::string message(filePath);
+    auto fileSize = std::to_string(st.st_size);
+    fileSize += " ";
+    fileSize += "LOLKEK ";
+    message += fileSize;
 
-    if( send(sock , y.c_str() , y.size() , 0) < 0) {
-        puts("Send failed");
-        return 1;
+    if (send(sock, message.c_str(), message.size(), 0) < 0) {
+        std::cerr << "Send failed" << std::endl;
+        return -1;
     }
-    std::fill(message,message+4096,0);
 
-    std::cout<<"dasdad\n";
+    std::cout << recv(sock, serverReply, SERVER_REPLY_BUFFER_SIZE, 0);
+    std::cout << serverReply;
 
-    std::cout<< recv(sock,server_reply,4096,0);
-    std::cout<<server_reply;
     FILE* fin = fopen("heh.txt","r");
-    size_t readed = 4096;
-    for (long long i = 0; readed ==4096 ; ++i) {
-        readed = fread(message, sizeof(char),4096,fin);
-        send(sock,message,readed,0);
-        std::cout<< readed<<std::endl;
+    size_t readed = SERVER_REPLY_BUFFER_SIZE;
+    for (long long i = 0; readed == SERVER_REPLY_BUFFER_SIZE; ++i) {
+        readed = fread(filePath, sizeof(char), SERVER_REPLY_BUFFER_SIZE, fin);
+        send(sock, filePath, readed, 0);
     }
-    recv(sock,server_reply,4096,0);
-    puts("Server reply :");
-    puts(server_reply);
+    std::fill(serverReply, serverReply + SERVER_REPLY_BUFFER_SIZE, 0);
+    recv(sock, serverReply, SERVER_REPLY_BUFFER_SIZE, 0);
+    std::cout << "Server reply: " << serverReply << std::endl;
     close(sock);
+    fclose(fin);
     return 0;
 }
