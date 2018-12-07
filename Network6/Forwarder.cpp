011c66f3e92767e1d2ba9f7a12661696f1312966
ankutalev <a.kutalev@g.nsu.ru>
2018-12-07 17:02:36 +0700

    Recv logic

--- Network6/Forwarder.cpp
+++ Network6/Forwarder.cpp
@@ -1,21 +1,28 @@
 #include "Forwarder.h"
 #include <unistd.h>
 #include <netdb.h>
+#include <algorithm>
 
 void Forwarder::acceptConnection() {
     sockaddr_in clientAddr;
     int size = sizeof(clientAddr);
 
-    auto client = accept(clientsDescryptors[0].fd, (sockaddr *) &clientAddr, (socklen_t *) &size);
-    pollfd c;
-    c.fd = client;
-    c.events = POLLIN;
-    clientsAddresses[client] = clientAddr;
-    clientsDescryptors.emplace_back(c);
+    auto client = accept(pollDescryptors[0].fd, (sockaddr *) &clientAddr, (socklen_t *) &size);
+    fcntl(client, F_SETFL, fcntl(client, F_GETFL, 0) | O_NONBLOCK);
+    //after connectToTarget() call this->targetSocket updated
+
+    if (!connectToTarget()) {
+        close(client);
+        return;
+    }
+
+    registerDescryptorForPollRead(client, &clientAddr, true);
+    registerDescryptorForPollRead(targetSocket, &targetInfo, false);
 
     char info[100];
     inet_ntop(AF_INET, &clientAddr, info, 100);
-    std::cout << "ACCEPTED descryptor " << client << " from IP ADDR" << info << std::endl;
+    std::cout << "ACCEPTED descryptor " << client << " from IP ADDR" << info << "IT'S TARGET SOCKEt IS " << targetSocket
+              << std::endl;
 }
 
 Forwarder::Forwarder(int myPort, std::string_view targetName, int tPort) : targetPath(targetName), port(myPort),
@@ -25,6 +32,7 @@ Forwarder::Forwarder(int myPort, std::string_view targetName, int tPort) : targe
     if (mySocket == -1)
         throw std::runtime_error("Can't open server socket!");
     int opt = 1;
+
     if (setsockopt(mySocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) or
         fcntl(mySocket, F_SETFL, fcntl(mySocket, F_GETFL, 0) | O_NONBLOCK) == -1)
         throw std::runtime_error("Can't make socket nonblocking!");
@@ -40,12 +48,29 @@ Forwarder::Forwarder(int myPort, std::string_view targetName, int tPort) : targe
     }
 
     listen(mySocket, MAX_CLIENTS);
-
+    std::fill(buff, buff + BUFFER_SIZE, 0);
     pollfd me;
     me.fd = mySocket;
     me.events = POLLIN;
-    clientsDescryptors.emplace_back(me);
+    pollDescryptors.emplace_back(me);
     clientsAddresses[mySocket] = forwarderInfo;
+
+
+    addrinfo hints = {0};
+    hints.ai_flags = 0;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    addrinfo *addr = nullptr;
+    if (getaddrinfo(targetPath.c_str(), nullptr, &hints, &addr)) {
+        std::cerr << " Can't resolve hostname!!! Terminating" << std::endl;
+    }
+
+    targetInfo = *(sockaddr_in *) (addr->ai_addr);
+    targetInfo.sin_family = AF_INET;
+    targetInfo.sin_port = htons(80);
+
+
 }
 
 Forwarder::~Forwarder() {
@@ -59,11 +84,18 @@ void Forwarder::startListen() {
 }
 
 void Forwarder::pollManage() {
-    auto changedDescryptors = poll(clientsDescryptors.data(), clientsDescryptors.size(), POLL_DELAY);
+    auto changedDescryptors = poll(pollDescryptors.data(), pollDescryptors.size(), POLL_DELAY);
+//    std::cerr<<"CHANGED DESCRYPTORS "<<changedDescryptors<<std::endl;
     for (int i = 0; i < changedDescryptors; ++i) {
-        for (auto &clientsDescryptor : clientsDescryptors) {
-            if (clientsDescryptor.revents == POLLIN)
-                acceptConnection();
+        for (auto &clientsDescryptor : pollDescryptors) {
+            if (clientsDescryptor.revents & POLLIN) {
+                if (clientsDescryptor.fd == mySocket) {
+                    acceptConnection();
+                } else {
+                    readData(clientsDescryptor.fd);
+                }
+            } else if (clientsDescryptor.revents & POLLOUT)
+                sendData(clientsDescryptor.fd);
         }
     }
 }
@@ -72,36 +104,96 @@ void Forwarder::pollManage() {
 Forwarder::Forwarder() : Forwarder(8080, "fit.ippolitov.me", 80) {}
 
 bool Forwarder::connectToTarget() {
-    addrinfo hints = {0};
-    hints.ai_flags = 0;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    addrinfo *addr = nullptr;
-    if (getaddrinfo(targetPath.c_str(), nullptr, &hints, &addr)) {
-        std::cerr << " Can't resolve hostname!!! Terminating" << std::endl;
-        return false;
-    }
-
-    targetInfo = *(sockaddr_in *) (addr->ai_addr);
-    targetInfo.sin_family = AF_INET;
-    targetInfo.sin_port = htons(80);
 
     targetSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (targetSocket == -1) {
         std::cerr << "Can't open target socket! Terminating" << std::endl;
         return false;
     }
+    if (connect(targetSocket, (sockaddr *) &targetInfo, sizeof(targetInfo))) {
+        perror("WO \n");
+        std::cerr << "Can't connect to target! Terminating!" << std::endl;
+        return false;
+    }
     if (fcntl(targetSocket, F_SETFL, fcntl(targetSocket, F_GETFL, 0) | O_NONBLOCK) == -1) {
         std::cerr << "Can't make target socket non blocking! Terminating!" << std::endl;
         return false;
     }
-    if (connect(targetSocket, (sockaddr *) &targetInfo, sizeof(targetInfo))) {
-        std::cerr << "Can't connect to target! Terminating!" << std::endl;
-        return false
-    }
 
     return true;
+}
+
+
+void Forwarder::readData(int from) {
+//    std::string data;
+    int to = transferPipes[from];
+    std::cerr << "READING";
+
+//    while(1) {
+    auto readed = recv(from, buff, BUFFER_SIZE - 1, 0);
+    std::cerr << readed << " " << (errno == EWOULDBLOCK) << std::endl;
+//        if (errno==EWOULDBLOCK)
+//            break;
+    if (!readed) {
+        close(from);
+        close(to);
+        transferPipes.erase(to);
+        transferPipes.erase(from);
+        eraseFromPoll(from);
+        eraseFromPoll(to);
+        return;
+    }
+//        buff[readed] = 0;
+//        data += buff;
+//    }
+//    std::cerr<<"Ready read";
+//    registerDescryptorForPollWrite(to);
+//    dataPieces[from] = data;
+}
+
+void Forwarder::sendData(int to) {
+//    int from = transferPipes[to];
+//    std::cerr<<"SENDING!\n";
+//    std::cout<<send(to, dataPieces[from].c_str(), dataPieces[from].size(), 0);
+//    std::cerr<<"sended!\n";
+//    std::cout<<dataPieces[from]<<std::endl;
+//    perror("wah");
+    if (to != 5)
+        exit(4);
+}
+
+void Forwarder::registerDescryptorForPollRead(int desc, sockaddr_in *addr, bool isClient) {
+
+    pollfd c;
+    c.fd = desc;
+    c.events = POLLIN;
+    pollDescryptors.emplace_back(c);
+
+    if (isClient) {
+        clientsAddresses[desc] = *addr;
+        transferPipes[targetSocket] = c.fd;
+        transferPipes[c.fd] = targetSocket;
+    }
+
+
+}
+
+void Forwarder::eraseFromPoll(int des) {
+    for (auto it = pollDescryptors.begin(); it != pollDescryptors.end();) {
+        if (it->fd == des) {
+            pollDescryptors.erase(it);
+            return;
+        } else ++it;
+    }
+}
+
+void Forwarder::registerDescryptorForPollWrite(int desc) {
+    for (auto it = pollDescryptors.begin(); it != pollDescryptors.end();) {
+        if (it->fd == desc) {
+            it->events |= POLLOUT;
+            return;
+        } else ++it;
+    }
 }
 
 
--- Network6/Forwarder.h
+++ Network6/Forwarder.h
@@ -16,19 +16,32 @@ public:
     void startListen();
     ~Forwarder();
 private:
+    void registerDescryptorForPollRead(int desc, sockaddr_in *addr, bool isClient);
+
+    void registerDescryptorForPollWrite(int desc);
+
+    void readData(int from);
+
+    void sendData(int to);
     void acceptConnection();
     void pollManage();
     bool connectToTarget();
+
+    void eraseFromPoll(int desc);
 private:
     static const int MAX_CLIENTS = 1024;
     static const int POLL_DELAY = 5000;
+    static const int BUFFER_SIZE = 50;
+    char buff[BUFFER_SIZE];
     int port = 8081;
-    int targetPort;
+    int targetPort = 80;
     int mySocket= -1;
     int targetSocket = -1;
     sockaddr_in targetInfo;
     sockaddr_in forwarderInfo;
-    std::string targetPath;
-    std::vector<pollfd> clientsDescryptors;
+    std::string targetPath = "fit.ippolitov.me";
+    std::vector<pollfd> pollDescryptors;
     std::unordered_map<int,sockaddr_in> clientsAddresses;
+    std::unordered_map<int, int> transferPipes;
+    std::unordered_map<int, std::string> dataPieces;
 };
