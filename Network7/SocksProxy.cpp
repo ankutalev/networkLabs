53ebb7a1c1561a61d73ad89f05a20593f8a200fe
ankutalev <a.kutalev@g.nsu.ru>
2019-01-05 02:46:41 +0700

    Ready version without DNS resolving (segfault was fixed.... maybe)

--- Network7/SocksProxy.cpp
+++ Network7/SocksProxy.cpp
@@ -14,9 +14,6 @@
 #include "SocksProxy.h"
 
 
-static void registerForWrite(pollfd* fd) {
-    fd->events = POLLOUT;
-}
 
 
 static void removeFromPoll(std::vector<pollfd>::iterator* it) {
@@ -29,16 +26,11 @@ static void removeFromPoll(std::vector<pollfd>::iterator* it) {
 }
 
 
-void CacheProxy::writeToClient(std::vector<pollfd>::iterator* clientIterator) {
-    std::cout << " TO DO";
-    removeFromPoll(clientIterator);
-}
-
 void CacheProxy::passIdentification(std::vector<pollfd>::iterator* clientIterator) {
     pollfd* client = &**clientIterator;
     std::fill(buffer, buffer + BUFFER_LENGTH, 0);
     auto readed = recv(client->fd, buffer, BUFFER_LENGTH, 0);
-    std::cout << "read in id" << readed << std::endl;
+    std::cout << "read in id " << readed << std::endl;
     buffer[readed] = 0;
 
     if (!checkSOCKSRequest(client->fd, readed)) {
@@ -51,31 +43,72 @@ void CacheProxy::passIdentification(std::vector<pollfd>::iterator* clientIterato
     char addrType = buffer[3];
 
     sockaddr_in targetAddr;
+    socklen_t addrSize = sizeof(targetAddr);
+    targetAddr.sin_family = AF_INET;
     targetAddr.sin_port = constructPort(buffer + 8);
     targetAddr.sin_addr.s_addr = constructIpAddr(buffer + 4);
-    targetAddr.sin_family = AF_INET;
-
-
+    std::cout << "target port " << targetAddr.sin_port << std::endl;
+    std::cout << "target addr " << inet_ntoa(targetAddr.sin_addr) << std::endl;
+    char response[] = {SOCKS_VERSION, SOCKS_SUCCESS, 0, IPv4_ADDRESS, 0, 0, 0, 0, 0, 0};
+    auto target = socket(AF_INET, SOCK_STREAM, 0);
+    if (target == -1) {
+        std::cout << "INVALID IP ADDR OR PORT GET!" << std::endl;
+        response[1] = SOCKS_SERVER_ERROR;
+        send(client->fd, response, sizeof(response), 0);
+        passedHandshake.erase(client);
+        removeFromPoll(clientIterator);
+        return;
+    }
+    fcntl(target, F_SETFL, fcntl(target, F_GETFL, 0) | O_NONBLOCK);
+    if (connect(target, (sockaddr*) &targetAddr, addrSize) and errno != EINPROGRESS) {
+        response[1] = HOST_NOT_REACHABLE;
+        send(client->fd, response, sizeof(response), 0);
+        passedHandshake.erase(client);
+        removeFromPoll(clientIterator);
+        return;
+    }
+    std::cout << "ya zaconnectilsya!" << std::endl;
+    send(client->fd, response, sizeof(response), 0);
+    pollfd fd;
+    fd.fd = target;
+    fd.events = POLLIN;
+    fd.revents = 0;
+    *clientIterator = pollDescryptors->insert(*clientIterator + 1, fd);
+    passedHandshake.erase(client);
+    passedFullSOCKSprotocol.insert(client);
+    passedFullSOCKSprotocol.insert(&**clientIterator);
+    (*transferMap)[client] = &**clientIterator;
+    (*transferMap)[&**clientIterator] = client;
 }
 
 void CacheProxy::makeHandshake(std::vector<pollfd>::iterator* clientIterator) {
     pollfd* fd = &**clientIterator;
     auto readed = recv(fd->fd, buffer, BUFFER_LENGTH, 0);
+    std::cout << "I READ IN HANDSHAKE" << readed << std::endl;
     buffer[readed] = 0;
-    printSocksBuffer(readed);
+//    printSocksBuffer(readed);f
+
     char response[2] = {SOCKS_VERSION, INVALID_AUTHORISATION};
+    if (buffer[0] != SOCKS_VERSION) {
+        send(fd->fd, response, HANDSHAKE_LENGTH, 0);
+        removeFromPoll(clientIterator);
+        return;
+    }
+
     int availableAuthorisations = buffer[1];
     for (int j = 0, i = 2; j < availableAuthorisations; ++i, ++j) {
         if (buffer[i] == SUPPORTED_AUTHORISATION) {
             std::cout << "supported auto! " << std::endl;
-            response[1] = 0;
+            response[1] = SUPPORTED_AUTHORISATION;
         }
     }
-    send(fd->fd, response, HANDSHAKE_LENGTH, 0);
 
-    auto addr = descToAddress[fd];
-    waitingForAuthorisaton.erase(fd);
-    passedAuthorisation.insert(addr);
+    send(fd->fd, response, HANDSHAKE_LENGTH, 0);
+    if (response[1] != SUPPORTED_AUTHORISATION) {
+        removeFromPoll(clientIterator);
+        return;
+    }
+    passedHandshake.insert(fd);
 }
 
 
@@ -89,20 +122,28 @@ void CacheProxy::acceptConnection(pollfd* client) {
     if (newClient == -1) {
         throw std::runtime_error("can't accept!");
     }
-    socklen_t size = sizeof(serverAddr);
-    if (not doIKnowOwnIp) {
-        getsockname(serverSocket, (sockaddr*) &serverAddr, &size);
-        std::cout << inet_ntoa(serverAddr.sin_addr) << std::endl;
-        doIKnowOwnIp = true;
-    }
     //is this required?
     fcntl(newClient, F_SETFL, fcntl(newClient, F_GETFL, 0) | O_NONBLOCK);
     client->fd = newClient;
 }
 
-void CacheProxy::sendData(std::vector<pollfd>::iterator* target) {
-    std::cout << " todo send data!" << std::endl;
-    return;
+void CacheProxy::sendData(std::vector<pollfd>::iterator* clientIterator) {
+    std::cout << " send data!" << std::endl;
+
+    auto client = &**clientIterator;
+    ssize_t sended = 0;
+    auto size = (*dataPieces)[client].size();
+    do {
+        sended = send(client->fd, (*dataPieces)[client].data(), (*dataPieces)[client].size(), 0);
+        (*dataPieces)[client].erase((*dataPieces)[client].begin(), (*dataPieces)[client].begin() + sended);
+    } while (sended > 0);
+    dataPieces->erase(client);
+    for (auto it = pollDescryptors->begin(); it != pollDescryptors->end();) {
+        if (&*it == client) {
+            it->events ^= POLLOUT;
+            return;
+        } else ++it;
+    }
 }
 
 CacheProxy::CacheProxy() {
@@ -127,30 +168,31 @@ void CacheProxy::pollManage() {
 
     auto cd = poll(&(*pollDescryptors)[0], pollDescryptors->size(), POLL_DELAY);
 //    std::cout<<cd <<std::endl;
-
+    std::cout << "NEW POLL CYCLE!!!!!!!!!" << std::endl;
     for (auto it = pollDescryptors->begin(); it != pollDescryptors->end(); ++it) {
         //если валидный
         std::cout << it->fd << " " << it->events << " " << it->revents << std::endl;
         if (it->fd > 0) {
             //если ошибка (конекшен рефьюзед)
             if (it->revents & POLLERR) {
-                pollfd* client = (*transferMap)[&*it];
-                close(client->fd);
-                client->fd = (-client->fd);
                 removeFromPoll(&it);
                 std::cout << "REFUSED" << std::endl;
             } else if (it->revents & POLLOUT) {
-                writeToClient(&it);
+                sendData(&it);
             } else if (it->revents & POLLIN) {
                 if (it->fd == serverSocket) {
                     std::cout << "connection " << std::endl;
                     acceptConnection(&c);
-                } else if (waitingForAuthorisaton.count(&*it)) {
-                    std::cout << "handshake " << std::endl;
-                    makeHandshake(&it);
-                } else if (passedAuthorisation.count(descToAddress[&*it])) {
-                    std::cout << "authorise" << std::endl;
+                } else if (passedFullSOCKSprotocol.count(&*it)) {
+
+                    readData(&it);
+                } else if (passedHandshake.count(&*it)) {
+                    std::cout << "socks authorise " << std::endl;
                     passIdentification(&it);
+                    std::cout << "pass" << std::endl;
+                } else {
+                    std::cout << "making handshake! " << std::endl;
+                    makeHandshake(&it);
                 }
             }
         }
@@ -158,11 +200,7 @@ void CacheProxy::pollManage() {
 
     if (c.fd != -1) {
         pollDescryptors->push_back(c);
-        SockAddrWrapper sock;
-        sock.addr = addr;
         auto fd = &*(pollDescryptors->end() - 1);
-        descToAddress[&*(pollDescryptors->end() - 1)] = sock;
-        waitingForAuthorisaton.insert(&*(pollDescryptors->end() - 1));
     }
 
     removeDeadDescryptors();
@@ -182,8 +220,8 @@ void CacheProxy::removeDeadDescryptors() {
     auto ndataPieces = new std::map<pollfd*, std::vector<char> >;
     std::map<pollfd*, pollfd*> oldNewMap;
 
-    std::unordered_map<pollfd*, SockAddrWrapper> newDescToAddr;
-    std::set<pollfd*> newWaiting;
+    std::unordered_set<pollfd*> newPassedHandshake;
+    std::unordered_set<pollfd*> newPassedSocks;
 
 
     for (auto &pollDescryptor : *pollDescryptors) {
@@ -191,10 +229,8 @@ void CacheProxy::removeDeadDescryptors() {
             npollDescryptors->push_back(pollDescryptor);
             oldNewMap[&pollDescryptor] = &npollDescryptors->back();
         }
-        if (waitingForAuthorisaton.count(&pollDescryptor))
-            newWaiting.insert(&npollDescryptors->back());
-
     }
+
 
 
     for (auto &it : *transferMap) {
@@ -204,18 +240,22 @@ void CacheProxy::removeDeadDescryptors() {
         }
     }
 
+    for (const auto &handshake : passedHandshake) {
+        if (handshake->fd > 0)
+            newPassedHandshake.insert(oldNewMap[handshake]);
+    }
+
+    for (const auto &sock : passedFullSOCKSprotocol) {
+        if (sock->fd > 0)
+            newPassedSocks.insert(oldNewMap[sock]);
+    }
+
 
     for (auto &dataPiece : *dataPieces) {
         if (dataPiece.first->fd > 0)
             (*ndataPieces)[oldNewMap[dataPiece.first]] = dataPiece.second;
     }
 
-    for (auto &fd : descToAddress) {
-        if (fd.first->fd > 0)
-            newDescToAddr[oldNewMap[fd.first]] = fd.second;
-    }
-
-    descToAddress.swap(newDescToAddr);
 
 
     dataPieces->swap(*ndataPieces);
@@ -227,8 +267,8 @@ void CacheProxy::removeDeadDescryptors() {
     pollDescryptors->swap(*npollDescryptors);
     delete npollDescryptors;
 
-    newWaiting.swap(waitingForAuthorisaton);
-
+    passedFullSOCKSprotocol.swap(newPassedSocks);
+    passedHandshake.swap(newPassedHandshake);
 }
 
 void CacheProxy::init(int port) {
@@ -289,6 +329,7 @@ bool CacheProxy::checkSOCKSRequest(int client, ssize_t len) {
     if (socksVersion != SOCKS_VERSION) {
         char notSupported[] = {SOCKS_VERSION, PROTOCOL_ERROR, 0};
         send(client, notSupported, sizeof(notSupported), 0);
+        std::cerr << "invalid socks request!" << std::endl;
         return false;
     }
     if (addrType != IPv4_ADDRESS) {
@@ -311,6 +352,38 @@ uint16_t CacheProxy::constructPort(char* port) {
 
 uint32_t CacheProxy::constructIpAddr(char* port) {
     return *reinterpret_cast<uint32_t*>(port);
+}
+
+void CacheProxy::readData(std::vector<pollfd>::iterator* clientIterator) {
+
+    auto client = &**clientIterator;
+    std::cout << "READ DATA" << std::endl;
+
+    if (!transferMap->count(client)) {
+        removeFromPoll(clientIterator);
+        return;
+    }
+    pollfd* to = (*transferMap)[client];
+
+    while (1) {
+        auto readed = recv(client->fd, buffer, BUFFER_LENGTH - 1, 0);
+        std::cout << "i read " << readed << std::endl;
+        if (!readed or (readed == -1 and errno != EWOULDBLOCK)) {
+            std::cout << "UJOHU!!!!!" << std::endl;
+            registerForWrite(to);
+            removeFromPoll(clientIterator);
+            return;
+        }
+        if (errno == EWOULDBLOCK) {
+            errno = EXIT_SUCCESS;
+            registerForWrite(to);
+            return;
+        }
+        buffer[readed] = 0;
+        for (int i = 0; i < readed; ++i) {
+            (*dataPieces)[to].emplace_back(buffer[i]);
+        }
+    }
 }
 
 
--- Network7/SocksProxy.h
+++ Network7/SocksProxy.h
@@ -11,12 +11,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-struct SockAddrWrapper {
-    bool operator<(const SockAddrWrapper &a) const { return addr.sin_port < a.addr.sin_port; }
-
-    sockaddr_in addr;
-};
-
 class CacheProxy {
 public:
     CacheProxy();
@@ -34,17 +28,17 @@ private:
 
     void init(int port);
 
-    void registerForWrite(pollfd* fd) { fd->events = POLLOUT; }
+    void registerForWrite(pollfd* fd) { fd->events |= POLLOUT; }
 
     void passIdentification(std::vector<pollfd>::iterator* clientIterator);
 
     void sendData(std::vector<pollfd>::iterator* clientIterator);
 
-    void writeToClient(std::vector<pollfd>::iterator* clientIterator);
-
     void acceptConnection(pollfd* client);
 
     void makeHandshake(std::vector<pollfd>::iterator* clientIterator);
+
+    void readData(std::vector<pollfd>::iterator* clientIterator);
 
     bool checkSOCKSRequest(int client, ssize_t len);
 
@@ -63,29 +57,29 @@ private:
     const static int BUFFER_LENGTH = 5000;
     const static int HANDSHAKE_LENGTH = 2;
     const static char SOCKS_VERSION = 5;
+    const static char SOCKS_SERVER_ERROR = 1;
     const static char INVALID_AUTHORISATION = 0xff;
     const static char SUPPORTED_AUTHORISATION = 0;
     const static char SUPPORTED_OPTION = 1;
     const static char IPv4_ADDRESS = 1;
     const static char OPTION_NOT_SUPPORTED = 7;
     const static char PROTOCOL_ERROR = 7;
+    const static char SOCKS_SUCCESS = 0;
     const static char ADDRESS_NOT_SUPPORTED = 8;
+    const static char HOST_NOT_REACHABLE = 4;
     const static int MINIMUM_SOCKS_REQUEST_LENGTH = 10;
     char buffer[BUFFER_LENGTH];
     sockaddr_in serverAddr;
-    bool doIKnowOwnIp = false;
     sockaddr_in addr;
     std::vector<pollfd>* pollDescryptors;
-    std::set<SockAddrWrapper> passedAuthorisation;
-    std::set<pollfd*> waitingForAuthorisaton;
-    std::unordered_map<pollfd*, SockAddrWrapper> descToAddress;
+
+    std::unordered_set<pollfd*> passedHandshake;
+
+    std::unordered_set<pollfd*> passedFullSOCKSprotocol;
     // кто куда пишет
     std::map<pollfd*, pollfd*>* transferMap;
-    // дескриптор -> путь на сайт и информацию является ли соединение клиентским
-
     //КОМУ что отправить
     std::map<pollfd*, std::vector<char> >* dataPieces;
-
 };
 
 
